69c889c2f9025b26a01c442f470f700bf6b12194
dlopez079 <dlopez079@gmail.com>
2019-10-11 19:48:46 -0400

    Completed the tasks in the 'AppSetup.txt' located in the root folder.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder', true);
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
