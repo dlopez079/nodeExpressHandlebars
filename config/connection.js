5d279316e4b8afc59f80a2040ce8fdce7e608f91
dlopez079 <dlopez079@gmail.com>
2019-10-14 19:06:33 -0400

    Created database on Heroku successfully.

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    } else {
+      connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'burgers_db'
+      })
+    }
+
+connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
